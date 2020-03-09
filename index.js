e1a023986ede8ed1a869a3edfc77ea1a8ef8c13a
Joe Gaudet <joe.gaudet@food.ee>
2020-03-09 11:46:03 -0700

    --wip-- [skip ci]

--- index.js
+++ index.js
@@ -71,7 +71,6 @@ const EXTRACTORS = {
     console.log('In: ');
     console.log(JSON.stringify(stack_trace, null, 2));
     let start = stack_trace.reverse().find(_ =>  _.includes(testRoot));
-
     console.log('Found: ' + start);
 
     let path = start.split(`${testRoot}/`)[1];
